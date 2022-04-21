bf6f42199b3aadd6b4e7511978bdb3a556b0b11e
sergei <sergei@home.168.1.7>
2022-04-21 16:38:22 +0300

    init: Старт тестового задания

---
+++ src/components/WCardItem.vue
@@ -0,0 +1,19 @@
+<template>
+<article class="w-card-item">
+  card
+  image: 
+</article>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+.w-card-item {
+  
+}
+</style>
