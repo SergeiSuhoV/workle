2b3c9963a86def548a75be44d18403011a79ce7c
sergei <sergei@home.168.1.7>
2022-04-26 20:54:16 +0300

    Настройка Github Pages

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:sergeisuhov/workle/.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  // Публикация в GP
+  publicPath: process.env.NODE_ENV === "production" ? "/workle/" : "/",
 });
