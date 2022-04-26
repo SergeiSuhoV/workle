dd78679ec2139b6bb1fbec1cf740b7e563bce763
sergei suhov <sergei@home.168.1.7>
2022-04-27 00:33:16 +0300

    init deploy dist

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  // Публикация в GP
-  publicPath: process.env.NODE_ENV === "production" ? "/workle/" : "/",
-});
